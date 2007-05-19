328ac8c9f992a0c56dbf46ee4aebc990b1135d52
tobinjt <tobinjt>
2007-05-19 13:48:03 +0000

    Wrote pod documenting all the ORM modules. Also changed Rule.pm over to %cols like the others.

--- ASO/DB.pm
+++ ASO/DB.pm
@@ -2,6 +2,84 @@
 
 # $Id$
 
+
+=head1 NAME
+
+ASO::DB - Load required database classes.
+
+=head1 VERSION
+
+This documentation refers to ASO::DB version $Id$
+
+
+=head1 SYNOPSIS
+
+    use ASO::DB;
+    my $dbix = ASO::DB->connect(
+        q{dbi:SQLite:dbname=../sql/db.sq3},
+        {AutoCommit => 0},
+    );
+    # Do stuff with $dbix - see DBIx::Class documentation.
+
+    foreach my $connection ($dbix->resultset(q{Connection})->search()) {
+        # Do something with $connection
+        print $connection->server_ip();
+    }
+
+    my $connection = $dbix->resultset(q{Connection})->new_result({
+        server_ip   => q{127.0.0.1}.
+        # etc. etc.
+    });
+
+=head1 DESCRIPTION
+
+This module wraps DBIx::Class and the modules representing each table so they 
+can be easily loaded.
+
+=head1 SUBROUTINES/METHODS 
+
+None.
+
+=head1 DIAGNOSTICS
+
+None.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None.
+
+=head1 DEPENDENCIES
+
+External packages: DBIx::Class::Schema.
+
+Bundled packages: ASO::DB::Connection, ASO::DB::Result, ASO::DB::Rule.
+
+=head1 INCOMPATIBILITIES
+
+None.
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module.  Please report problems to John Tobin
+<tobinjt@cs.tcd.ie>.  Patches are welcome.
+
+=head1 AUTHOR
+
+John Tobin <tobinjt@cs.tcd.ie>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2006-2007 John Tobin <tobinjt@cs.tcd.ie>.  All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+=cut
+
 use strict;
 use warnings;
 
--- ASO/DB/Base.pm
+++ ASO/DB/Base.pm
@@ -2,6 +2,51 @@
 
 # $Id$
 
+=head1 NAME
+
+ASO::DB::Base - Base class for ORM modules.
+
+=head1 VERSION
+
+This documentation refers to ASO::DB::Base version $Id$
+
+=head1 SYNOPSIS
+
+    package ASO::DB::Something;
+    use base q{DBIx::Class};
+    use base q{ASO::DB::Base};
+
+    my %cols = (
+        # columns and attributes
+    );
+
+    sub get_cols {
+        my ($self) = @_;
+        return %cols;
+    }
+
+    # Add any other methods here.
+
+    __PACKAGE__->load_components(qw(PK::Auto Core));
+    __PACKAGE__->table(q{TABLE});
+    __PACKAGE__->add_columns(
+        keys %cols
+    );
+    __PACKAGE__->set_primary_key(q{PRIMARY KEY});
+    # Foreign keys: other tables reference this one.
+    __PACKAGE__->has_many(q{results}            => q{ASO::DB::Result}
+                                                => q{connection_id});
+    1;
+
+=head1 DESCRIPTION
+
+Base class for ORM modules containing some methods used by some or all
+subclasses.
+
+=head1 SUBROUTINES/METHODS 
+
+=cut
+
 use strict;
 use warnings;
 
@@ -13,26 +58,70 @@ use Carp;
 our $pointless_variable;
 $pointless_variable = 1;
 
+=over 4
+
+=item $self->get_cols()
+
+In subclasses it returns the hash reference listing the columns in the table and
+their attributes; it must be overridden as the base version just dies.  In
+general the hash returned should not be modified.
+
+=back
+
+=cut
+
 sub get_cols {
     confess qq{get_cols: must be implemented by derived class\n};
 }
 
+=over 4
+
+=item $self->col_grep($attribute)
+
+Returns a hash reference containing the subset of the table columns which have
+$attribute set.
+
+=back
+
+=cut
+
 sub col_grep {
-    my ($self, $tag) = @_;
+    my ($self, $attribute) = @_;
     my %cols = $self->get_cols();
     my %results;
     foreach my $key (keys %cols) {
-        if (exists $cols{$key}->{$tag}) {
-            $results{$key} = $cols{$key}->{$tag};
+        if (exists $cols{$key}->{$attribute}) {
+            $results{$key} = $cols{$key}->{$attribute};
         }
     }
     return \%results;
 }
 
+=over 4
+
+=item $self->required_columns()
+
+Returns the columns which are required in the table, i.e. inserting a row
+without missing any of these columns is an error.
+
+=back
+
+=cut
+
 sub required_columns {
     my ($self) = @_;
     return $self->col_grep(q{required});
 }
+
+=over 4
+
+=item $self->nochange_columns()
+
+Returns the columns whose values, once set, should not change.
+
+=back
+
+=cut
 
 sub nochange_columns {
     my ($self) = @_;
@@ -40,3 +129,46 @@ sub nochange_columns {
 }
 
 1;
+
+=head1 DIAGNOSTICS
+
+None.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None.
+
+=head1 DEPENDENCIES
+
+Standard Perl modules: Carp.
+
+Bundled modules: ASO::DB::Base.
+
+External modules: DBIX::Class.
+
+=head1 INCOMPATIBILITIES
+
+None.
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module.  Please report problems to John Tobin
+<tobinjt@cs.tcd.ie>.  Patches are welcome.
+
+=head1 AUTHOR
+
+John Tobin <tobinjt@cs.tcd.ie>.
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2006-2007 John Tobin <tobinjt@cs.tcd.ie>.  All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+=cut
+
