e202b51f2f53daefc4bb6ab827a0213868bf6452
tobinjt <tobinjt>
2007-07-18 19:53:08 +0000

    Documented these modules.

--- ASO/ProgressBar.pm
+++ ASO/ProgressBar.pm
@@ -2,6 +2,98 @@
 
 # $Id$
 
+=head1 NAME
+
+ASO::ProgressBar - display a progress bar if Term::ProgressBar is available, do
+nothing if it isn't.
+
+=head1 VERSION
+
+This documentation refers to ASO::ProgressBar version $Id$
+
+=head1 SYNOPSIS
+
+    use ASO::ProgressBar;
+    my $progressbar = ASO::ProgressBar->new(100);
+    foreach my $i (1 .. 100) {
+        $progressbar->update($i);
+    }
+
+=head1 DESCRIPTION
+
+ASO::ProgressBar wraps Term::ProgressBar if it's available, or
+ASO::ProgressBar::Dummy if not.  This allows users who don't have
+Term::ProgressBar to use the parser without problems, e.g. on Windows.
+
+=head1 SUBROUTINES/METHODS 
+
+See Term::ProgressBar for full details, ASO::ProgressBar::Dummy for the
+guaranteed minimal interface.
+
+=head1 DIAGNOSTICS
+
+=over 4
+
+=item __PACKAGE__: failed to load any of: <list of modules>
+
+No usable module was found for this module to wrap.  This means your
+installation is broken in some fashion, because ASO::ProgressBar::Dummy is
+packaged with this module.
+
+=item __PACKAGE__: 'use $module' succeeded, but 'use base $module' didn't: $@
+
+For some reason there was no error from 'use $module;', but 'use base $module;'
+failed.  This really shouldn't happen - hopefully there'll be some more
+information in the error message.
+
+=back
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+No configuration required.
+
+ASO::ProgressBar::Dummy should be automatically installed, but it doesn't
+provide a progress bar (it's a dummy implementation).
+
+If Term::ProgressBar is available it will be used automatically.
+
+=head1 DEPENDENCIES
+
+Modules packaged with ASO: ASO::ProgressBar::Dummy.
+
+Optional external modules: Term::ProgressBar.
+
+=head1 INCOMPATIBILITIES
+
+None known thus far.
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module. 
+Please report problems to John Tobin (<tobinjt@cs.tcd.ie>).
+Patches are welcome.
+
+=head1 SEE ALSO
+
+Term::ProgressBar, ASO::ProgressBar::Dummy.
+
+=head1 AUTHOR
+
+John Tobin <tobinjt@cs.tcd.ie>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2006-2007 John Tobin <tobinjt@cs.tcd.ie>.  All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+=cut
+
 use strict;
 use warnings;
 
@@ -23,7 +115,7 @@ foreach my $module (@modules) {
     eval qq{use base q($module);};
     if ($@) {
         die qq{__PACKAGE__: 'use $module' succeeded, }
-            . qq{but 'use base $module' didn't.: $@\n};
+            . qq{but 'use base $module' didn't: $@\n};
     }
     $module_loaded = 1;
     last MODULE;
--- ASO/ProgressBar/Dummy.pm
+++ ASO/ProgressBar/Dummy.pm
@@ -2,14 +2,45 @@
 
 # $Id$
 
+=head1 NAME
+
+ASO::ProgressBar::Dummy - implements a minimal subset of Term::ProgressBar's
+API, but doesn't display a progress bar.
+
+=head1 VERSION
+
+This documentation refers to ASO::ProgressBar::Dummy version $Id$
+
+=head1 SYNOPSIS
+
+    # You shouldn't actually be using this module directly; use ASO::ProgressBar
+    # instead.
+
+=head1 DESCRIPTION
+
+ASO::ProgressBar::Dummy implements a minimal subset of Term::ProgressBar's API.
+This allows users who don't have Term::ProgressBar to use the parser without
+problems, e.g. on Windows.
+
+=head1 SUBROUTINES/METHODS 
+
+=cut
+
 use strict;
 use warnings;
 
 package ASO::ProgressBar::Dummy;
 
-# A dummy implementation of the API we need from Term::ProgressBar.
+=over 4
 
+=item ASO::ProgressBar::Dummy->new($max), ASO::ProgressBar::Dummy->new({count => $max})
 
+Takes either a number specifying the hightest value in the progress bar, or a
+hash of options - of those options, only count is used.
+
+=back
+
+=cut
 
 sub new {
     my $self = {};
@@ -25,6 +56,20 @@ sub new {
     return $self;
 }
 
+=over 4
+
+=item $progressbar->update($current)
+
+Change how far along the progress bar the caller has gotten, in the same units
+as the maximum passed to new().  Returns the next value update() should be
+called at.  No check is performed to ensure that it is less than the max value;
+if it isn't, the return value will probably be weird.  This doesn't display a
+progress bar, at all.
+
+=back
+
+=cut
+
 sub update {
     my ($self, $count) = @_;
     $self->{count} = $count;
@@ -34,9 +79,65 @@ sub update {
     return $next_count;
 }
 
+=over 4
+
+=item $progressbar->target($max)
+
+Changes the highest value of the progress bar.  No check is performed to ensure
+that it is greater than the current value.
+
+=back
+
+=cut
+
 sub target {
     my ($self, $max) = @_;
     $self->{max} = $max;
 }
+
+=head1 DIAGNOSTICS
+
+No intentional diagnotics, though it will probably spew warnings if you abuse
+the arguments given to methods.  It may also die pass a zero to new(), so don't
+do that.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+None.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None known thus far.
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module. 
+Please report problems to John Tobin (<tobinjt@cs.tcd.ie>).
+Patches are welcome.
+
+=head1 SEE ALSO
+
+Term::ProgressBar, ASO::ProgressBar.
+
+=head1 AUTHOR
+
+John Tobin <tobinjt@cs.tcd.ie>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2006-2007 John Tobin <tobinjt@cs.tcd.ie>.  All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+=cut
 
 1;
