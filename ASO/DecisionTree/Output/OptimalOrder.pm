a43ff37ad5f8f3901e2f27477323743a21f97e32
tobinjt <tobinjt>
2008-05-19 22:34:25 +0000

    First stab at optimal ordering, but I'm getting weird results. Will debug and pore over data tomorrow.

--- ASO/DecisionTree/Output/OptimalOrder.pm
+++ ASO/DecisionTree/Output/OptimalOrder.pm
@@ -5,6 +5,8 @@ package ASO::DecisionTree::Output::OptimalOrder;
 use warnings;
 use strict;
 use Carp;
+use Data::Dumper;
+use List::Util qw(sum);
 
 =head1 NAME
 
@@ -26,12 +28,14 @@ our ($VERSION) = q{$Id$} =~ m/(\d+)/mx;
 
     my @list = ASO::DecisionTree::Output::OptimalOrder->optimal_order($tree);
     print map { qq{$_->{restriction_name}, # $_->{percentage}\n} } @list;
-    # This is equivalent to the previous two lines.
+    # This produces similar output.
     ASO::DecisionTree::Output::OptimalOrder->print_optimal_order($tree);
 
 =head1 METHODS
 
-=head2 my @list = $ASO::DecisionTree::Output::OptimalOrder->optimal_order($tree, %options)
+=cut
+
+=head2 my @list = ASO::DecisionTree::Output::OptimalOrder->optimal_order($tree, %options)
 
 Returns a list of hash references, optimally ordered to reject mail as soon as
 possible, respecting cluster groups and the requirement to apply less stringent
@@ -70,6 +74,130 @@ will be included.
 =back
 
 =cut
+
+sub optimal_order {
+    my ($package, $tree, %options) = @_;
+
+    %options = $package->validate_options(%options);
+    # As far as I can see the optimal order is always going to be the rightmost
+    # path down the tree, i.e. taking the true branch everywhere.
+    my $total = $package->get_num_rows($tree);
+
+    my @restrictions;
+    my $node = $tree;
+    NODE:
+    while (1) {
+        if ($options{ignore_info_nodes} and $node->is_info()) {
+            $node = $node->info_branch();
+            next NODE;
+        }
+        my %restriction = (
+            total               => $total,
+            restriction_name    => $node->label(),
+        );
+        if ($node->is_info()) {
+            $restriction{count}      = 0;
+            $restriction{percentage} = 0;
+            push @restrictions, \%restriction;
+            $node = $node->info_branch();
+            next NODE;
+        }
+        if ($node->is_leaf()) {
+            $restriction{count}      = $package->get_num_rows($node);
+            $restriction{percentage} = $restriction{count} * 100 / $total;
+            push @restrictions, \%restriction;
+            last NODE;
+        }
+        if ($node->is_branch()) {
+            my $true_child = $node->true_branch();
+            $restriction{count}      =   $package->get_num_rows($node)
+                                       - $package->get_num_rows($true_child);
+            $restriction{percentage} = $restriction{count} * 100 / $total;
+            push @restrictions, \%restriction;
+            $node = $true_child;
+            next NODE;
+        }
+        croak qq{optimal_order: cannot deal with node: } . Dumper($node);
+    }
+
+    return @restrictions;
+}
+
+=head2 ASO::DecisionTree::Output::OptimalOrder->print_optimal_order($tree, %options)
+
+Prints the results of optimal_order() to the default print filehandle (C<STDOUT>
+unless you've changed it with C<select>).  Each hash returned will be formatted
+as:
+
+    $restriction_name, # $percentage%, $percentage_so_far
+
+=cut
+
+sub print_optimal_order {
+    my ($package, $tree, %options) = @_;
+
+    my @list = $package->optimal_order($tree, %options);
+    my $percentage_so_far = 0;
+    foreach my $result (@list) {
+        $percentage_so_far += $result->{percentage};
+        printf qq{%s, # %.4f%%, %.4f\n},    $result->{restriction_name},
+                                            $result->{percentage},
+                                            $percentage_so_far;
+    }
+}
+
+=head1 HELPER METHODS
+
+These are helper methods which shouldn't be called directly, they're used
+internally.
+
+=cut
+
+=head2 ASO::DecisionTree::Output::OptimalOrder->get_num_rows($tree)
+
+Returns the number of rows in $tree.  Will C<croak> on failure.
+
+=cut
+
+sub get_num_rows {
+    my ($package, $subtree) = @_;
+
+    if ($subtree->is_info()) {
+        return $package->get_num_rows($subtree->info_branch());
+    }
+    if ($subtree->is_leaf()) {
+        return sum(map { $_->{count} } @{$subtree->leaf_branch()}) || 0;
+    }
+    if ($subtree->is_branch()) {
+        return    $package->get_num_rows($subtree->true_branch())
+                + $package->get_num_rows($subtree->false_branch());
+    }
+    croak qq{get_num_rows: unable to deal with tree: } . Dumper($subtree);
+}
+
+=head2 ASO::DecisionTree::Output::OptimalOrder->validate_options(%options)
+
+Validate options passed to optimal_order() or print_optimal_order().  Returns
+%options if valid, will C<croak> otherwise.
+
+=cut
+
+sub validate_options {
+    my ($package, %options) = @_;
+
+    my %defaults = (
+        ignore_info_nodes   => 0,
+    );
+
+    foreach my $arg (keys %options) {
+        if (not exists $defaults{$arg}) {
+            croak qq{optimal_order: unknown option $arg\n};
+        }
+    }
+    %options = (%defaults, %options);
+
+    return %options;
+}
 
 =head1 AUTHOR
 
@@ -111,7 +239,7 @@ L<http://search.cpan.org/dist/ASO-DecisionTree-Output-OptimalOrder>
 
 =head1 DEPENDENCIES
 
-Standard modules bundled with Perl: L<Carp>.
+Standard modules bundled with Perl: L<Carp>, L<Data::Dumper>, L<List::Util>.
 
 Modules bundled with ASO: none.
 
