119a351f77511c6f4e5f8e6acd3cd101f162a3ab
tobinjt <tobinjt>
2008-05-20 22:44:04 +0000

    Added new() and converted everything else to expect an object.

--- ASO/DecisionTree/Output/Image.pm
+++ ASO/DecisionTree/Output/Image.pm
@@ -24,70 +24,108 @@ our ($VERSION) = q{$Id$} =~ m/(\d+)/mx;
 ASO::DecisionTree::Output::Image converts an L<ASO::DecisionTree> to an image
 for easy viewing.
 
-    my $decision_tree = ....;
-    my $image = ASO::DecisionTree::Output::Image->draw_tree($decision_tree);
-    # Do something with $image
-    # ....
-    # Save the image
-    ASO::DecisionTree::Output::Image->save_image($image, $filename);
+    my $decision_tree = ....; # See ASO::DecisionTree.
+    my $image = ASO::DecisionTree::Output::Image->new(
+        fontfile => q{/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf}
+    );
+    $image->draw_tree($decision_tree);
+    $image->save_image($filename);
 
 =head1 METHODS
 
-=head2 my $image = ASO::DecisionTree::Output::Image->draw_tree($decision_tree)
+=head2 my $image = ASO::DecisionTree::Output::Image->new(%options)
 
-Returns an image representing $decision_tree; $image is an L<Imager> object.
-Will C<croak> if anything fails.
+Create a new image object.  Takes the following arguments:
+
+=over 4
+
+=item fontfile => filename
+
+The font to use in the image.  Picking a good font is difficult; I suggest
+F</usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf> if you have it.
+This is a required option.
+
+=back
+
+=cut
+
+sub new {
+    my ($package, %options) = @_;
+
+    my %defaults = (
+        height_between_nodes => 50,
+        # XXX use bounding_box() at some stage.
+        height_of_label      => 15,
+    );
+
+    my %required = (
+        fontfile    => undef,
+    );
+
+    foreach my $required (keys %required) {
+        if (not exists $options{$required}) {
+            croak qq{new: required option $required is missing\n};
+        }
+    }
+    foreach my $option (keys %options) {
+        if (not exists $required{$option} and not exists $defaults{$option}) {
+            croak qq{new: unknown option $option\n};
+        }
+    }
+
+    %options = (%defaults, %options);
+
+    bless \%options, $package;
+    return \%options;
+}
+
+=head2 $image->draw_tree($decision_tree)
+
+Draw an image representing $decision_tree, based on the options given to new();
+will C<croak> if anything fails.
 
 =cut
 
 sub draw_tree {
-    my ($package, $tree) = @_;
+    my ($self, $tree) = @_;
 
     if (@_ != 2) {
         my $num_args = @_ - 1;
         croak qq{draw_tree(): expecting one argument, not $num_args\n};
     }
 
-    my $tree_height = $package->get_tree_height($tree);
+    my $tree_height = $self->get_tree_height($tree);
 print qq{tree height: $tree_height\n};
-    my $tree_width  = $package->get_tree_width($tree);
+    my $tree_width  = $self->get_tree_width($tree);
 print qq{tree width: $tree_width\n};
 
-    my $height_between_nodes = 50;
-    # XXX use bounding_box() at some stage.
-    my $height_of_label      = 15;
-    my $height_per_node      = $height_between_nodes + $height_of_label;
+    my $height_per_node      = $self->{height_between_nodes}
+                                + $self->{height_of_label};
     my $image_height         = ($height_per_node * ($tree_height - 1))
-                               + $height_of_label;
+                               + $self->{height_of_label};
     # XXX use 8 as a reasonable approximation; use bounding_box() at some stage.
     my $image_width          = $tree_width * 8;
-    print qq{image size: $image_width * $image_height\n};
+print qq{image size: $image_width * $image_height\n};
 
-    # XXX allow the user to specify this
-    my $fontfile = q{/usr/share/fonts/truetype/ttf-bitstream-vera/VeraMono.ttf};
-
-    my $image = Imager->new(xsize => $image_width, ysize => $image_height)
+    $self->{image} = Imager->new(xsize => $image_width, ysize => $image_height)
         or croak q{Failed creating image: } . Imager->errstr() . qq{\n};
-    $image->box(filled => 1, color => 'white');
-    my $font = Imager::Font->new(file => $fontfile)
-        or croak qq{failed to load $fontfile: } . Imager->errstr() . qq{\n};
+    $self->{image}->box(filled => 1, color => 'white');
+    $self->{font} = Imager::Font->new(file => $self->{fontfile})
+        or croak qq{failed to load $self->{fontfile}: }
+            . Imager->errstr() . qq{\n};
 
-    $package->draw_tree_r(
-        image                   => $image,
-        font                    => $font,
+    $self->draw_tree_r(
         tree                    => $tree,
         xmin                    => 0,
         ymin                    => 0,
         xmax                    => $image_width,
         ymax                    => $image_height,
-        height_of_label         => $height_of_label,
-        height_between_nodes    => $height_between_nodes,
     );
 
-    return $image;
+    return;
 }
 
-=head2 ASO::DecisionTree::Output::Image->save_image($image, $filename)
+=head2 $image->save_image($filename)
 
 Saves $image to $filename; the output type is determined by the extension of
 $filename.  Will C<croak> if saving the image fails.
@@ -95,16 +133,16 @@ $filename.  Will C<croak> if saving the image fails.
 =cut
 
 sub save_image {
-    my ($package, $image, $filename) = @_;
+    my ($self, $filename) = @_;
 
-    if (@_ != 3) {
+    if (@_ != 2) {
         my $num_args = @_ - 1;
-        croak qq{save_image(): expecting two arguments, not $num_args\n};
+        croak qq{save_image(): expecting one argument, not $num_args\n};
     }
 
-    $image->write(file => $filename)
+    $self->{image}->write(file => $filename)
         or croak qq{Failed writing image: $filename: }
-                 . $image->errstr() . qq{\n};
+                 . $self->{image}->errstr() . qq{\n};
 }
 
 =head1 INTERNAL METHODS
@@ -114,24 +152,22 @@ you may need to replace them.
 
 =cut
 
-=head2 ASO::DecisionTree::Output::Image->draw_tree_r(%args)
+=head2 $image->draw_tree_r(%args)
 
-XXX Recursively draw a tree.
+Recursively draw a tree.
 
 =cut
 
 sub draw_tree_r {
-    my ($package, %args) = @_;
+    my ($self, %args) = @_;
 
     if ($args{tree}->{leaf_node} or $args{tree}->{info_node}) {
         # Draw the label.
         my $label_x = ($args{xmin} + $args{xmax}) / 2;
-        $package->draw_label(
-            string     => $package->get_label($args{tree}),
+        $self->draw_label(
+            string     => $self->get_label($args{tree}),
             x          => $label_x,
             y          => $args{ymin},
-            image      => $args{image},
-            font       => $args{font},
         );
         if ($args{tree}->{leaf_node}) {
             # Finished with leaf nodes.
@@ -139,9 +175,9 @@ sub draw_tree_r {
         }
 
         # Draw the remainder of the tree.
-        my $child_ymin = $args{ymin} + $args{height_of_label}
-                                     + $args{height_between_nodes};
-        my ($child_x, $child_y) = $package->draw_tree_r(
+        my $child_ymin = $args{ymin} + $self->{height_of_label}
+                                     + $self->{height_between_nodes};
+        my ($child_x, $child_y) = $self->draw_tree_r(
             %args,
             tree       => $args{tree}->{info_branch},
             xmin       => $args{xmin},
@@ -151,10 +187,9 @@ sub draw_tree_r {
         );
 
         # Draw the connecting line for info node.
-        my $line_y = $args{ymin} + $args{height_of_label};
+        my $line_y = $args{ymin} + $self->{height_of_label};
         my $line_x = $label_x;
-        $package->draw_line(
-            image   => $args{image},
+        $self->draw_line(
             x1      => $line_x,
             y1      => $line_y,
             x2      => $child_x,
@@ -166,20 +201,20 @@ sub draw_tree_r {
     }
 
     # Displaying true/false nodes is enough code to go into a separate function.
-    return $package->draw_tree_true_false_node(%args);
+    return $self->draw_tree_true_false_node(%args);
 }
 
-=head2 ASO::DecisionTree::Output::Image->draw_tree_r(%args)
+=head2 $image->draw_tree_r(%args)
 
-XXX Draw a node with true and false branches.
+Draw a node with true and false branches.
 
 =cut
 
 sub draw_tree_true_false_node {
-    my ($package, %args) = @_;
+    my ($self, %args) = @_;
 
-    my $false_width = $package->get_tree_width($args{tree}->{false_branch});
-    my $true_width  = $package->get_tree_width($args{tree}->{true_branch});
+    my $false_width = $self->get_tree_width($args{tree}->{false_branch});
+    my $true_width  = $self->get_tree_width($args{tree}->{true_branch});
     my $total_width = $false_width + $true_width;
 
     my $xsize       = $args{xmax} - $args{xmin};
@@ -189,7 +224,7 @@ sub draw_tree_true_false_node {
     # Need to ensure the label doesn't extend past xmin or xmax; this can
     # happen if half the label width is greater than the width of one of the
     # branches.  In that case shift it left or right enough to fit in the box.
-    my $half_label_length = $package->get_label_length($args{tree}) / 2;
+    my $half_label_length = $self->get_label_length($args{tree}) / 2;
     my $left_label_width  = max($false_width, $half_label_length);
     if ($half_label_length > $true_width) {
         $left_label_width  = $total_width - $half_label_length;
@@ -197,18 +232,16 @@ sub draw_tree_true_false_node {
     my $label_x = $args{xmin} + ($xsize * $left_label_width / $total_width);
 
     # Add the label.
-    $package->draw_label(
-        string     => $package->get_label($args{tree}),
+    $self->draw_label(
+        string     => $self->get_label($args{tree}),
         x          => $label_x,
         y          => $args{ymin},
-        image      => $args{image},
-        font       => $args{font},
     );
 
-    my $child_ymin = $args{ymin} + $args{height_of_label}
-                                 + $args{height_between_nodes};
+    my $child_ymin = $args{ymin} + $self->{height_of_label}
+                                 + $self->{height_between_nodes};
     # Draw the false branch
-    my ($false_line_x, $false_line_y) = $package->draw_tree_r(
+    my ($false_line_x, $false_line_y) = $self->draw_tree_r(
         %args,
         tree       => $args{tree}->{false_branch},
         xmin       => $args{xmin},
@@ -218,7 +251,7 @@ sub draw_tree_true_false_node {
     );
 
     # Draw the true branch
-    my ($true_line_x, $true_line_y) = $package->draw_tree_r(
+    my ($true_line_x, $true_line_y) = $self->draw_tree_r(
         %args,
         tree       => $args{tree}->{true_branch},
         xmin       => $args{xmin} + $false_xsize,
@@ -228,42 +261,36 @@ sub draw_tree_true_false_node {
     );
 
     # Draw the connecting lines
-    my $line_y = $args{ymin} + $args{height_of_label};
+    my $line_y = $args{ymin} + $self->{height_of_label};
     my $line_x = $label_x;
-    $package->draw_line(
-        image   => $args{image},
+    $self->draw_line(
         x1      => $line_x,
         y1      => $line_y,
         x2      => $false_line_x,
         y2      => $false_line_y,
     );
 
-    $package->draw_line(
-        image   => $args{image},
+    $self->draw_line(
         x1      => $line_x,
         y1      => $line_y,
         x2      => $true_line_x,
         y2      => $true_line_y,
     );
 
-    my $line_label_y = $line_y + ($args{height_between_nodes} / 2) - 2;
+    my $line_label_y = $line_y + ($self->{height_between_nodes} / 2) - 2;
     # Label the lines.
-    $package->draw_label(
+    $self->draw_label(
         string      => q{0},
         x           => (($line_x + $false_line_x) / 2) - 2,
         y           => $line_label_y,
-        image       => $args{image},
-        font        => $args{font},
         halign      => q{right},
         valign      => q{bottom},
     );
 
-    $package->draw_label(
+    $self->draw_label(
         string      => q{1},
         x           => (($line_x + $true_line_x) / 2) + 2,
         y           => $line_label_y,
-        image       => $args{image},
-        font        => $args{font},
         halign      => q{left},
         valign      => q{bottom},
     );
@@ -271,14 +298,14 @@ sub draw_tree_true_false_node {
     return ($label_x, $args{ymin});
 }
 
-=head2 ASO::DecisionTree::Output::Image->draw_line(%args)
+=head2 $image->draw_line(%args)
 
-XXX Draw a line on the image.
+Draw a line on the image.
 
 =cut
 
 sub draw_line {
-    my ($package, %args) = @_;
+    my ($self, %args) = @_;
     my %defaults = (
         color   => q{blue},
         aa      => 1,
@@ -286,124 +313,124 @@ sub draw_line {
     );
     %args = (%defaults, %args);
 
-    my $image = delete $args{image};
-    $image->line(%args)
-        or die qq{Drawing line failed: } . $image->errstr() . qq{\n};
+    $self->{image}->line(%args)
+        or die qq{Drawing line failed: } . $self->{image}->errstr() . qq{\n};
 }
 
-=head2 ASO::DecisionTree::Output::Image->draw_label(%args)
+=head2 $image->draw_label(%args)
 
-XXX Draw a label on the image.
+Draw a label on the image.
 
 =cut
 
 sub draw_label {
-    my ($package, %args) = @_;
+    my ($self, %args) = @_;
     my %defaults = (
         size       => 12,
         color      => q{black},
         valign     => q{top},
         halign     => q{center},
+        image      => $self->{image},
     );
     %args = (%defaults, %args);
 
-    my $font = delete $args{font};
-    $font->align(%args)
-        or die qq{Drawing label failed: } . $args{image}->errstr() . qq{\n};
+    $self->{font}->align(%args)
+        or die qq{Drawing label failed: } . $self->{image}->errstr() . qq{\n};
 }
 
-=head2 ASO::DecisionTree::Output::Image->get_tree_height(%args)
+=head2 $image->get_tree_height(%args)
 
-XXX Recursively determine the height of the tree.
+Recursively determine the height of the tree.  This is the number of nodes from
+top to bottom, not the height of the image.
 
 =cut
 
 sub get_tree_height {
-    my ($package, $subtree) = @_;
+    my ($self, $subtree) = @_;
 
     if ($subtree->{leaf_node}) {
         return 1;
     }
 
     if ($subtree->{info_node}) {
-        return 1 + $package->get_tree_height($subtree->{info_branch});
+        return 1 + $self->get_tree_height($subtree->{info_branch});
     }
 
-    my $false_height = $package->get_tree_height($subtree->{false_branch});
-    my $true_height  = $package->get_tree_height($subtree->{true_branch});
+    my $false_height = $self->get_tree_height($subtree->{false_branch});
+    my $true_height  = $self->get_tree_height($subtree->{true_branch});
     return max($false_height, $true_height) + 1;
 }
 
-=head2 ASO::DecisionTree::Output::Image->get_tree_width(%args)
+=head2 $image->get_tree_width(%args)
 
-XXX Determine the width of the tree; will possibly over estimate it, depending
-on the shape of the tree.
+Determine the width of the tree; will possibly over estimate it, depending on
+the shape of the tree.
 
 =cut
 
 sub get_tree_width {
-    my ($package, $subtree) = @_;
+    my ($self, $subtree) = @_;
 
-    my $label_width = $package->get_label_length($subtree);
+    my $label_width = $self->get_label_length($subtree);
     if ($subtree->{leaf_node}) {
         return $label_width;
     }
 
     if ($subtree->{info_node}) {
-        my $child_width = $package->get_tree_width($subtree->{info_branch});
+        my $child_width = $self->get_tree_width($subtree->{info_branch});
         return max($label_width, $child_width);
     }
 
-    my $false_width = $package->get_tree_width($subtree->{false_branch});
-    my $true_width  = $package->get_tree_width($subtree->{true_branch});
+    my $false_width = $self->get_tree_width($subtree->{false_branch});
+    my $true_width  = $self->get_tree_width($subtree->{true_branch});
     my $child_width = $false_width + $true_width;
     return max($child_width, $label_width);
 }
 
-=head2 ASO::DecisionTree::Output::Image->get_label_length($tree)
+=head2 $image->get_label_length($tree)
 
 Returns the width of the label for the top node of $tree.
 
 =cut
 
 sub get_label_length {
-    my ($package, $subtree) = @_;
+    my ($self, $subtree) = @_;
 
-    my $label = $package->get_label($subtree);
+    my $label = $self->get_label($subtree);
     # Add some padding to separate labels.
     return 2 + length $label;
 }
 
-=head2 ASO::DecisionTree::Output::Image->get_label($tree)
+=head2 $image->get_label($tree)
 
 Returns the label for the top node of $tree.
 
 =cut
 
 sub get_label {
-    my ($package, $subtree) = @_;
+    my ($self, $subtree) = @_;
 
-    my $num_rows = $package->get_num_rows($subtree);
+    my $num_rows = $self->get_num_rows($subtree);
     return qq{$subtree->{label} ($num_rows)};
 }
 
-=head2 ASO::DecisionTree::Output::Image->get_num_rows($tree)
+=head2 $image->get_num_rows($tree)
 
 Returns the number of rows in $tree.
 
 =cut
 
 sub get_num_rows {
-    my ($package, $subtree) = @_;
+    my ($self, $subtree) = @_;
 
     if ($subtree->{info_node}) {
-        return $package->get_num_rows($subtree->{info_branch});
+        return $self->get_num_rows($subtree->{info_branch});
     }
     if ($subtree->{leaf_node}) {
         return sum(map { $_->{count} } @{$subtree->{leaf_branch}}) || 0;
     }
-    return    $package->get_num_rows($subtree->{true_branch})
-            + $package->get_num_rows($subtree->{false_branch});
+    return    $self->get_num_rows($subtree->{true_branch})
+            + $self->get_num_rows($subtree->{false_branch});
 }
 
 =head1 AUTHOR
