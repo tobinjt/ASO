6c73703fd85ed9d1aab9f7b5c44e272b250c5992
tobinjt <tobinjt>
2008-05-21 18:39:47 +0000

    Made the required changes to create a left to right tree. Tuned the tree so it looks quite well.

--- ASO/DecisionTree/Output/ImageLR.pm
+++ ASO/DecisionTree/Output/ImageLR.pm
@@ -58,9 +58,22 @@ This is a required option.
 The size of font to use; 12 is a reasonable start, though your font may require
 a larger or smaller size.  This is a required argument.
 
-=item width_between_nodes => integer
+=item width_between_branch_nodes => integer
 
-The width between nodes in the tree.  Default is 50 pixels.
+The width of the gap between branch nodes in the tree.  Default is 100 pixels.
+
+=item width_between_info_nodes => integer
+
+The width of the gap between info nodes in the tree.  Default is 50 pixels.
+
+=item label_padding => integer
+
+The amount of padding to put before and after labels.  Default is 10
+pixels.
+
+=item height_between_nodes => integer
+
+The height of the gap between nodes in the tree.  Default is 10 pixels.
 
 =back
 
@@ -84,32 +97,32 @@ sub draw_tree_r {
 
     if ($args{tree}->{leaf_node} or $args{tree}->{info_node}) {
         # Draw the label.
-        my $label_x = ($args{xmin} + $args{xmax}) / 2;
+        my $label_y = ($args{ymin} + $args{ymax}) / 2;
         $self->draw_label(
             string     => $self->get_label($args{tree}),
-            x          => $label_x,
-            y          => $args{ymin},
+            x          => $args{xmin} + $self->{label_padding},
+            y          => $label_y,
         );
         if ($args{tree}->{leaf_node}) {
             # Finished with leaf nodes.
-            return ($label_x, $args{ymin});
+            return ($args{xmin}, $label_y);
         }
 
         # Draw the remainder of the tree.
-        my $child_ymin = $args{ymin} + $self->get_label_height($args{tree})
-                                     + $self->{width_between_nodes};
+        my $child_xmin = $args{xmin} + $self->get_label_width($args{tree})
+                                     + $self->{width_between_info_nodes};
         my ($child_x, $child_y) = $self->draw_tree_r(
             %args,
             tree       => $args{tree}->{info_branch},
-            xmin       => $args{xmin},
-            ymin       => $child_ymin,
+            xmin       => $child_xmin,
+            ymin       => $args{ymin},
             xmax       => $args{xmax},
             ymax       => $args{ymax},
         );
 
         # Draw the connecting line for info node.
-        my $line_y = $args{ymin} + $self->get_label_height($args{tree});
-        my $line_x = $label_x;
+        my $line_x = $args{xmin} + $self->get_label_width($args{tree});
+        my $line_y = $label_y;
         $self->draw_line(
             x1      => $line_x,
             y1      => $line_y,
@@ -118,7 +131,7 @@ sub draw_tree_r {
         );
 
         # Finished with info nodes.
-        return ($label_x, $args{ymin});
+        return ($args{xmin}, $label_y);
     }
 
     # Displaying true/false nodes is enough code to go into a separate function.
@@ -134,52 +147,45 @@ Draw a node with true and false branches.
 sub draw_tree_true_false_node {
     my ($self, %args) = @_;
 
-    my $false_width = $self->get_tree_width($args{tree}->{false_branch});
-    my $true_width  = $self->get_tree_width($args{tree}->{true_branch});
-    my $total_width = $false_width + $true_width;
+    my $false_height = $self->get_tree_height($args{tree}->{false_branch});
+    my $true_height  = $self->get_tree_height($args{tree}->{true_branch});
+    my $total_height = $self->get_tree_height($args{tree});
 
-    # Need to ensure the label doesn't extend past xmin or xmax; this can
-    # happen if half the label width is greater than the width of one of the
-    # branches.  In that case shift it left or right enough to fit in the box.
-    my $half_label_length = $self->get_label_width($args{tree}) / 2;
-    my $left_label_width  = max($false_width, $half_label_length);
-    if ($half_label_length > $true_width) {
-        $left_label_width  = $total_width - $half_label_length;
-    }
-    my $label_x = $args{xmin} + $left_label_width;
+    my $label_y = $args{ymin} + $true_height
+                    + ($self->{height_between_nodes} / 2);
 
     # Add the label.
     $self->draw_label(
         string     => $self->get_label($args{tree}),
-        x          => $label_x,
-        y          => $args{ymin},
+        x          => $args{xmin} + $self->{label_padding},
+        y          => $label_y,
     );
 
-    my $child_ymin = $args{ymin} + $self->get_label_height($args{tree})
-                                 + $self->{width_between_nodes};
-    # Draw the false branch
+    my $child_xmin = $args{xmin} + $self->get_label_width($args{tree})
+                                 + $self->{width_between_branch_nodes};
+    # Draw the false branch - on the bottom
     my ($false_line_x, $false_line_y) = $self->draw_tree_r(
         %args,
         tree       => $args{tree}->{false_branch},
-        xmin       => $args{xmin},
-        ymin       => $child_ymin,
-        xmax       => $args{xmin} + $false_width,
-        ymax       => $args{ymax},
-    );
-
-    # Draw the true branch
-    my ($true_line_x, $true_line_y) = $self->draw_tree_r(
-        %args,
-        tree       => $args{tree}->{true_branch},
-        xmin       => $args{xmin} + $false_width,
-        ymin       => $child_ymin,
+        xmin       => $child_xmin,
+        ymin       => $args{ymax} - $false_height,
         xmax       => $args{xmax},
         ymax       => $args{ymax},
     );
 
+    # Draw the true branch - on the top
+    my ($true_line_x, $true_line_y) = $self->draw_tree_r(
+        %args,
+        tree       => $args{tree}->{true_branch},
+        xmin       => $child_xmin,
+        ymin       => $args{ymin},
+        xmax       => $args{xmax},
+        ymax       => $args{ymin} + $true_height,
+    );
+
     # Draw the connecting lines
-    my $line_y = $args{ymin} + $self->get_label_height($args{tree});
-    my $line_x = $label_x;
+    my $line_y = $label_y;
+    my $line_x = $args{xmin} + $self->get_label_width($args{tree});
     $self->draw_line(
         x1      => $line_x,
         y1      => $line_y,
@@ -194,25 +200,25 @@ sub draw_tree_true_false_node {
         y2      => $true_line_y,
     );
 
-    my $line_label_y = $line_y + ($self->{width_between_nodes} / 2) - 2;
+    my $line_label_x = $line_x + ($self->{width_between_branch_nodes} / 2) - 2;
     # Label the lines.
     $self->draw_label(
         string      => q{0},
-        x           => (($line_x + $false_line_x) / 2) - 2,
-        y           => $line_label_y,
+        x           => $line_label_x,
+        y           => (($line_y + $false_line_y) / 2) + 2,
         halign      => q{right},
-        valign      => q{bottom},
+        valign      => q{top},
     );
 
     $self->draw_label(
         string      => q{1},
-        x           => (($line_x + $true_line_x) / 2) + 2,
-        y           => $line_label_y,
-        halign      => q{left},
+        x           => $line_label_x,
+        y           => (($line_y + $true_line_y) / 2) - 2,
+        halign      => q{right},
         valign      => q{bottom},
     );
 
-    return ($label_x, $args{ymin});
+    return ($args{xmin}, $label_y);
 }
 
 =head2 $image->get_tree_height(%args)
@@ -230,14 +236,15 @@ sub get_tree_height {
     }
 
     if ($subtree->{info_node}) {
-        return $label_height + $self->{width_between_nodes}
-                + $self->get_tree_height($subtree->{info_branch});
+        my $child_height = $self->get_tree_height($subtree->{info_branch});
+        return max($label_height, $child_height);
     }
 
     my $false_height = $self->get_tree_height($subtree->{false_branch});
     my $true_height  = $self->get_tree_height($subtree->{true_branch});
-    return $label_height + $self->{width_between_nodes}
-            + max($false_height, $true_height);
+    my $child_height = $false_height + $self->{height_between_nodes}
+                        + $true_height;
+    return max($label_height, $child_height);
 }
 
 =head2 $image->get_tree_width(%args)
@@ -257,13 +264,13 @@ sub get_tree_width {
 
     if ($subtree->{info_node}) {
         my $child_width = $self->get_tree_width($subtree->{info_branch});
-        return max($label_width, $child_width);
+        return $label_width + $self->{width_between_info_nodes} + $child_width;
     }
 
     my $false_width = $self->get_tree_width($subtree->{false_branch});
     my $true_width  = $self->get_tree_width($subtree->{true_branch});
-    my $child_width = $false_width + $true_width;
-    return max($child_width, $label_width);
+    return $label_width + $self->{width_between_branch_nodes}
+            + max($false_width, $true_width);
 }
 
 =head2 ASO::DecisionTree::Output::ImageLR->get_default_options()
@@ -276,9 +283,43 @@ sub get_default_options {
     my ($package) = @_;
 
     my %defaults = (
-        width_between_nodes => 50,
+        width_between_branch_nodes  => 100,
+        width_between_info_nodes    => 50,
+        height_between_nodes        => 10,
+        label_padding               => 10,
     );
     return %defaults;
+}
+
+=head2 $image->draw_label(%args)
+
+Draw a label on the image.
+
+=cut
+
+sub draw_label {
+    my ($self, %args) = @_;
+
+    # Alter the interpretation of the coordinates.
+    my %defaults = (
+        valign     => q{center},
+        halign     => q{left},
+    );
+    %args = (%defaults, %args);
+    return $self->SUPER::draw_label(%args);
+}
+
+=head2 $image->get_label_width($tree)
+
+Returns the width of the label, including label_padding, for the top node of $tree.
+
+=cut
+
+sub get_label_width {
+    my ($self, $subtree) = @_;
+
+    my ($height, $width) = $self->get_label_size($subtree);
+    return $width + ($self->{label_padding} * 2);
 }
 
 =head1 AUTHOR
