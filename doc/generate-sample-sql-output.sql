a2d18a84037a2cafb85959345cda22411db9528b
tobinjt <tobinjt>
2009-02-25 22:23:59 +0000

    Corrections to the background chapter.

--- doc/generate-sample-sql-output.sql
+++ doc/generate-sample-sql-output.sql
@@ -2,10 +2,10 @@ SELECT restriction_name, hits_total,
         (hits_total * 100.0 / 
             (SELECT SUM(hits_total)
                 FROM rules
-                WHERE postfix_action = 'REJECTED'
+                WHERE action = 'DELIVERY_REJECTED'
             )
         ) || '%' AS percentage
     FROM rules
-    WHERE postfix_action = 'REJECTED'
+    WHERE action = 'DELIVERY_REJECTED'
     ORDER BY hits_total DESC
     LIMIT 10;
